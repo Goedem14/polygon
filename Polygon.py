48f65330e747281a6f0b3f310029ba4c846d3fbb
Goekhan <101545820+Goedem14@users.noreply.github.com>
2022-05-27 11:32:37 +0200

    Add files via upload

--- Polygon.py
+++ Polygon.py
@@ -10,7 +10,10 @@ def polygon (n,length):
 
 def main ():
     """Draw polygon with 3-9 sides."""
-    for n in range (3,10):
+    t.up()
+    t.goto(-150,-140)
+    t.down()
+    for n in range (3,15):
         polygon(n,80)
     turtle.exitonclick()
 
